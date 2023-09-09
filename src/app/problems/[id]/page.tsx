816ab1fda8040084c82f5122850bbda43477eb10
Zhenya <evgeegai@gmail.com>
2023-09-09 15:13:53 +0300

    get ids for ssgand ssg from Firebase

--- src/app/problems/[id]/page.tsx
+++ src/app/problems/[id]/page.tsx
@@ -1,12 +1,44 @@
-import { mockDB } from '@/app/mocks/singleProblem';
 import { Splits } from '@/app/components/Splits/Splits';
 import { notFound } from 'next/navigation';
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
+import { fireStore } from '@/firebase/firebase';
+import { Problem } from '@/app/types';
+
+export const revalidate = 60*60*24;
 
 export async function generateStaticParams() {
-  return [{ id: '1' }, { id: '2' }];
+  const querySnapshot = await getDocs(collection(fireStore, 'approved-problems-ids'));
+  const params: {id: string}[] = [];
+  querySnapshot.forEach((doc) => {
+    params.push({id: doc.id});
+  });
+
+  return params;
 }
 
-export default function SingleProblem({ params }: { params: {id: string}}){
-  if(!mockDB[params.id]) notFound()
-  return <Splits problem={mockDB[params.id]}/>
+export async function getProblemData(id: string): Promise< Problem | null>{
+  const docRef = doc(fireStore, `problems/${id}`);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    return {
+      id: docSnap.id,
+      title: docSnap.data().title,
+      statement: docSnap.data().statement,
+      category: docSnap.data().category,
+      difficulty: docSnap.data().difficulty,
+      likes: docSnap.data().likes,
+      constraints: docSnap.data().constraints,
+      handler: docSnap.data().handler,
+      starter: docSnap.data().starter
+    }
+  } else {
+    return null;
+  }
+}
+
+export default async function SingleProblem({ params }: { params: {id: string}}){
+  const problemData = await getProblemData(params.id);
+  if(!problemData) notFound();
+  return <Splits problem={problemData}/>
 }
